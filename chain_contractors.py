dac7cf1fc7dd5078c34b4f12c4eeafcdff3da7a0
Gregory Crosswhite <gcross@phys.washington.edu>
2009-09-30 19:22:06 -0700

    Added site optimization functionality w/ tests to the expectation chain class.

--- chain_contractors.py
+++ chain_contractors.py
@@ -5,7 +5,9 @@
 #@<< Import needed modules >>
 #@+node:gcross.20090930134608.1306:<< Import needed modules >>
 from utils import make_contractor_from_implicit_joins
-from numpy import inner
+from numpy import inner, complex128, argsort, real, isfinite
+from scipy.sparse.linalg.eigen.arpack import eigen
+from scipy.sparse.linalg import aslinearoperator
 #@-node:gcross.20090930134608.1306:<< Import needed modules >>
 #@nl
 
@@ -185,8 +187,44 @@ partially_contract_overlap = make_contractor_from_implicit_joins([
     21,
 ])
 #@-node:gcross.20090930134608.1336:Partial contraction for overlap
+#@+node:gcross.20090930134608.1360:Compute optimization matrix
+#@+at
+# 
+# L = left boundary
+# R = left boundary
+# S = state site tensor
+# O = operator site tensor
+# 
+# /-1-   -21-\
+# |    |     |
+# |    32    |
+# |    |     |
+# L-2--O--22-R
+# |    |     |
+# |    42    |
+# |    |     |
+# \-3-   -23-/
+# 
+#@-at
+#@@c
+
+compute_optimization_matrix = make_contractor_from_implicit_joins([
+    [32,42,2,22],  # operator site tensor
+    [1,2,3],       # left boundary tensor
+    [21,22,23],    # right boundary tensor
+],[
+    (32,1,21),
+    (42,3,23),
+])
+#@-node:gcross.20090930134608.1360:Compute optimization matrix
 #@-node:gcross.20090930124443.1285:Contractors
 #@-node:gcross.20090930124443.1294:Functions
+#@+node:gcross.20090930134608.1354:Exceptions
+#@+node:gcross.20090930134608.1356:class ConvergenceError
+class ConvergenceError(Exception):
+    pass
+#@-node:gcross.20090930134608.1356:class ConvergenceError
+#@-node:gcross.20090930134608.1354:Exceptions
 #@+node:gcross.20090930124443.1293:Classes
 #@+others
 #@+node:gcross.20090930134608.1341:ChainContractorBase
@@ -197,11 +235,12 @@ class ChainContractorBase(object):
         "left_boundary","left_boundary_stack","left_operator_stack",
         "right_boundary","right_boundary_stack","right_operator_stack",
         "operator_site_tensor",
-        "number_of_sites","current_site_number"
+        "number_of_sites","current_site_number",
+        "physical_dimension","bandwidth_dimension",
     ]
     #@-node:gcross.20090930134608.1289:__slots__
     #@+node:gcross.20090930134608.1323:__init__
-    def __init__(self,initial_left_boundary,initial_right_boundary,number_of_sites):
+    def __init__(self,initial_left_boundary,initial_right_boundary,number_of_sites,physical_dimension):
         self.left_boundary = initial_left_boundary
         self.right_boundary = initial_right_boundary
         self.number_of_sites = number_of_sites
@@ -210,6 +249,11 @@ class ChainContractorBase(object):
         self.left_operator_stack = []
         self.right_boundary_stack = []
         self.right_operator_stack = []
+        self.physical_dimension = physical_dimension
+        bandwidth_dimension = initial_left_boundary.shape[0]
+        self.bandwidth_dimension = bandwidth_dimension
+        self.state_site_vector_size = physical_dimension * bandwidth_dimension**2
+        self.state_site_tensor_shape = (physical_dimension,bandwidth_dimension,bandwidth_dimension)
     #@-node:gcross.20090930134608.1323:__init__
     #@+node:gcross.20090930134608.1263:push_XXX_boundary
     def push_left_boundary(self):
@@ -253,7 +297,7 @@ class ChainContractorForExpectations(ChainContractorBase):
     #@    @+others
     #@+node:gcross.20090930134608.1273:__init__
     def __init__(self,initial_left_boundary,initial_right_boundary,initial_state_site_tensors,operator_site_tensors):
-        ChainContractorBase.__init__(self,initial_left_boundary,initial_right_boundary,len(initial_state_site_tensors))
+        ChainContractorBase.__init__(self,initial_left_boundary,initial_right_boundary,len(initial_state_site_tensors),initial_state_site_tensors[0].shape[0])
         assert(self.number_of_sites == len(operator_site_tensors))
         for i in xrange(self.number_of_sites-1,0,-1):
             self.operator_site_tensor = operator_site_tensors[i]
@@ -291,6 +335,101 @@ class ChainContractorForExpectations(ChainContractorBase):
             )
 
     #@-node:gcross.20090930134608.1338:partially_contract_with_state_site_tensor
+    #@+node:gcross.20090930134608.1349:compute_optimized_site_matrix
+    def compute_optimized_state_site_tensor(self,guess=None,iteration_cap=None,tol=0,energy_raise_threshold=1e-10,k=1,ncv=None,sigma_solve=None):
+        n = self.state_site_vector_size
+        state_site_tensor_shape = self.state_site_tensor_shape
+        class matrix(object):
+            shape = (n,n)
+            dtype = complex128
+            @staticmethod
+            def matvec(state_site_vector):
+                return self.partially_contract_with_state_site_tensor(state_site_vector.reshape(state_site_tensor_shape)).ravel()
+        try:
+            eigenvalues, eigenvectors = \
+                eigen(
+                    matrix,1,
+                    which='SR',
+                    ncv=ncv,
+                    v0=guess.ravel() if guess is not None else None,
+                    maxiter=iteration_cap,
+                    tol=tol,
+                    return_eigenvectors=True
+                )
+        except ValueError, msg:
+            raise ConvergenceError, "Problem converging to solution in eigenvalue solver: "+str(msg)
+
+        eigenvectors = eigenvectors.transpose()
+
+    #@+at
+    # It's not enough to simply take the eigenvector corresponding to the 
+    # lowest eigenvalue as our solution, since sometimes for various reasons 
+    # (due mainly to numerical instability) some or all of the eigenvalues are 
+    # bogus.  For example, sometimes the algorithm returns th the equivalent 
+    # of $-\infty$, which generally corresponds to an eigenvector that is very 
+    # nearly zero.  Thus, it is important to filter out all of these bogus 
+    # solutions before declaring victory.
+    #@-at
+    #@@c
+
+        sorted_indices_of_eigenvalues = argsort(real(eigenvalues))
+
+    #@+at
+    # A solution $\lambda,\vx$ is acceptable iff:
+    # 
+    # \begin{itemize}
+    # \item $\|\vx\|_\infty > 1\cdot 10^{-10}$, to rule out erroneous ``nearly 
+    # zero'' solutions that result in infinitely large eigenvalues
+    # \item $\abs{\text{Im}[\lambda]} < 1\cdot 10^{-10}$, since energies 
+    # should be \emph{real} as the Hamiltonian is hermitian.  (Small imaginary 
+    # parts aren't great, but they don't hurt too much as long as they are 
+    # sufficiently negligible.)
+    # \item $\abs{x_i}<\infty, \abs{\lambda} < \infty$
+    # \item $\abs{\frac{\coip{\vx}{\bD}{\vx}}{\coip{\vx}{\bN}{\vx}} - 
+    # \lam}<1\cdot 10^{-10}$, since sometimes the returned eigenvalue is 
+    # actually much different from the energy obtained by using the 
+    # corresponding eigenvector
+    # \end{itemize}
+    #@-at
+    #@@c
+        def is_acceptable(index):
+            evec = eigenvectors[index]
+            eval = eigenvalues[index]
+            return max(abs(evec)) > 1e-10 and isfinite(evec).all() and isfinite(eval)
+
+        acceptable_indices_of_eigenvalues = filter(is_acceptable,sorted_indices_of_eigenvalues)
+        if len(acceptable_indices_of_eigenvalues) == 0:
+            print eigenvalues
+            print eigenvectors
+            raise ConvergenceError, "All eigenvectors had near-vanishing normals, eigenvalues with large imaginary parts, or NaNs and/or infs."
+
+    #@+at
+    # If we've reached this point, then we have at least one solution that 
+    # isn't atrocious.  However, it's possible that our solution has a higher 
+    # energy than our current state -- either due to a problem in the 
+    # eigenvalue solver, or because we filtered out all the lower-energy 
+    # solutions.  Thus, we need compare the energy of the solution we've found 
+    # to our old energy to make sure that it's higher.
+    #@-at
+    #@@c
+
+        index_of_solution = acceptable_indices_of_eigenvalues[0]
+        state_site_tensor = eigenvectors[index_of_solution].reshape(state_site_tensor_shape)
+
+        new_energy = real(eigenvalues[index_of_solution])
+
+        if guess is not None:
+            old_energy = self.fully_contract_with_state_site_tensor(guess.conj(),guess)/inner(guess.conj().ravel(),guess.ravel())
+
+            if old_energy is not None and real(new_energy) > real(old_energy) and abs(new_energy-old_energy)>energy_raise_threshold and False:
+                raise ConvergenceError, "Only found a solution which *raised* the energy. (%.15f < %.15f)" % (new_energy,old_energy)
+
+        return state_site_tensor, new_energy
+    #@-node:gcross.20090930134608.1349:compute_optimized_site_matrix
+    #@+node:gcross.20090930134608.1361:compute_optimization_matrix
+    def compute_optimization_matrix(self):
+        return compute_optimization_matrix(self.operator_site_tensor,self.left_boundary,self.right_boundary)
+    #@-node:gcross.20090930134608.1361:compute_optimization_matrix
     #@-others
 #@-node:gcross.20090930124443.1295:ChainContractorForExpectations
 #@+node:gcross.20090930134608.1292:ChainContractorForOverlaps
@@ -298,7 +437,7 @@ class ChainContractorForOverlaps(ChainContractorBase):
     #@    @+others
     #@+node:gcross.20090930134608.1294:__init__
     def __init__(self,initial_left_boundary,initial_right_boundary,initial_state_A_site_tensors,initial_state_B_site_tensors):
-        ChainContractorBase.__init__(self,initial_left_boundary,initial_right_boundary,len(initial_state_A_site_tensors))
+        ChainContractorBase.__init__(self,initial_left_boundary,initial_right_boundary,len(initial_state_A_site_tensors),initial_state_A_site_tensors[0].shape[0])
         self.operator_site_tensor = None
         self.right_operator_stack = [None]*(self.number_of_sites-1)
         for i in xrange(self.number_of_sites-1,0,-1):
@@ -341,6 +480,7 @@ if __name__ == '__main__':
     from utils import crand, normalize
     from paulis import *
     from numpy import identity, allclose, array, zeros
+    from numpy.linalg import norm
     from paycheck import *
     #@    @+others
     #@+node:gcross.20090930134608.1326:ChainContractorForExpectationsTests
@@ -413,6 +553,51 @@ if __name__ == '__main__':
                 chain.contract_and_move_left(initial_site_tensor,initial_site_tensor)
                 self.assertEqual(number_of_sites,chain.fully_contract_with_state_site_tensor(initial_site_tensor,initial_site_tensor))
         #@-node:gcross.20090930134608.1333:testOnSpinStateInMagneticZField
+        #@+node:gcross.20090930134608.1353:testOptimizerOnSpinState1InMagneticField
+        @with_checker
+        def testOptimizerOnSpinState1InMagneticField(self,number_of_sites=irange(2,6)):
+            initial_left_boundary = array([1,0],dtype=complex128).reshape(1,2,1)
+            initial_right_boundary = array([0,1],dtype=complex128).reshape(1,2,1)
+            initial_site_tensor = array([0,0,1],dtype=complex128).reshape(3,1,1)
+            initial_site_tensors = [initial_site_tensor]*number_of_sites
+            operator_site_tensor = zeros((3,3,2,2),dtype=complex128)
+            operator_site_tensor[...,0,0] = array(identity(3),dtype=complex128)
+            operator_site_tensor[...,0,1] = array([[1,0,0],[0,0,0],[0,0,-1]],complex128)
+            operator_site_tensor[...,1,1] = array(identity(3),dtype=complex128)
+            operator_site_tensors = [operator_site_tensor]*number_of_sites
+            chain = ChainContractorForExpectations(initial_left_boundary,initial_right_boundary,initial_site_tensors,operator_site_tensors)
+
+            def check():
+                new_tensor, new_energy = chain.compute_optimized_state_site_tensor()
+                self.assertAlmostEqual(-number_of_sites,new_energy)
+                self.assertTrue(allclose(abs(new_tensor.ravel())/norm(new_tensor.ravel()),array([0,0,1])))
+
+            check()
+
+            for i in xrange(number_of_sites-1):
+                chain.contract_and_move_right(initial_site_tensor,initial_site_tensor)
+                check()
+
+            for i in xrange(number_of_sites-1):
+                chain.contract_and_move_left(initial_site_tensor,initial_site_tensor)
+                check()
+
+
+            def check():
+                new_tensor, new_energy = chain.compute_optimized_state_site_tensor(guess=initial_site_tensor.copy())
+                self.assertAlmostEqual(-number_of_sites,new_energy)
+                self.assertTrue(allclose(new_tensor.ravel(),array([0,0,1])))
+
+            check()
+
+            for i in xrange(number_of_sites-1):
+                chain.contract_and_move_right(initial_site_tensor,initial_site_tensor)
+                check()
+
+            for i in xrange(number_of_sites-1):
+                chain.contract_and_move_left(initial_site_tensor,initial_site_tensor)
+                check()
+        #@-node:gcross.20090930134608.1353:testOptimizerOnSpinState1InMagneticField
         #@-others
     #@nonl
     #@-node:gcross.20090930134608.1326:ChainContractorForExpectationsTests
@@ -422,6 +607,7 @@ if __name__ == '__main__':
         #@+node:gcross.20090930134608.1317:testOnNormalizedState
         @with_checker
         def testOnNormalizedState(self,physical_dimension=irange(2,4),bandwidth_dimension=irange(2,4),number_of_sites=irange(2,6)):
+            number_of_sites = 2
             initial_left_boundary = initial_right_boundary = identity(bandwidth_dimension)
             initial_site_tensors = [normalize(crand(physical_dimension,bandwidth_dimension,bandwidth_dimension),1) for _ in xrange(number_of_sites)]
             chain = ChainContractorForOverlaps(initial_left_boundary,initial_right_boundary,initial_site_tensors,initial_site_tensors)
