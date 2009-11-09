6b9a2cbc419f946f430b9ae775d713a132e4f3dd
Gregory Crosswhite <gcross@phys.washington.edu>
2009-11-08 17:46:47 -0800

    Separate desparsification core into a separate function

--- tests/contractors.py
+++ tests/contractors.py
@@ -16,6 +16,16 @@ from utils import *
 import vmps
 
 #@+others
+#@+node:gcross.20091108152444.1533:Functions
+#@+node:gcross.20091108152444.1535:sparse_to_operator
+def sparse_to_operator(c,d,sparse_operator_indices,sparse_operator_matrices):
+    operator = zeros((d,d,c,c),complex128)
+    for (index1,index2),matrix in zip(sparse_operator_indices.transpose(),sparse_operator_matrices.transpose(2,0,1)):
+        operator[...,index1-1,index2-1] += matrix
+    return operator
+#@-node:gcross.20091108152444.1535:sparse_to_operator
+#@-node:gcross.20091108152444.1533:Functions
+#@+node:gcross.20091108152444.1534:Tests
 #@+node:gcross.20091106154604.1986:pre_iteration
 pre_iteration_correct_contractor = make_contractor_from_implicit_joins([
     [1,2,3],       # left environment
@@ -34,9 +44,7 @@ class pre_iteration(unittest.TestCase):
         sparse_operator_indices = array([(randint(1,c),randint(1,c)) for _ in xrange(randint(2,2*c))]).transpose()
         sparse_operator_matrices = array(array([random()*choice([I,X,Y,Z]) for _ in xrange(sparse_operator_indices.shape[-1])],dtype=complex128).transpose(1,2,0),order='Fortran')
         actual_output_tensor = vmps.contractors.pre_iteration(left_environment,sparse_operator_indices,sparse_operator_matrices)
-        operator = zeros((d,d,c,c),complex128)
-        for (index1,index2),matrix in zip(sparse_operator_indices.transpose(),sparse_operator_matrices.transpose(2,0,1)):
-            operator[...,index1-1,index2-1] += matrix
+        operator = sparse_to_operator(c,d,sparse_operator_indices,sparse_operator_matrices)
         correct_output_tensor = pre_iteration_correct_contractor(left_environment,operator)
         self.assertTrue(allclose(actual_output_tensor,correct_output_tensor))
 #@-node:gcross.20091106154604.1986:pre_iteration
@@ -63,6 +71,7 @@ class iteration(unittest.TestCase):
         correct_output_tensor = iteration_correct_contractor(iteration_tensor,state_site_tensor,right_environment)
         self.assertTrue(allclose(actual_output_tensor,correct_output_tensor))
 #@-node:gcross.20091107163338.1531:iteration
+#@-node:gcross.20091108152444.1534:Tests
 #@-others
 
 tests = [
