09a848fba28c4f591001dae85d33bba7676d259c
Gregory Crosswhite <gcross@phys.washington.edu>
2009-11-11 22:31:29 -0800

    Implemented and tested the haskell wrapper around the optimization routine!!!

--- src/VMPS.hs
+++ src/VMPS.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 -- @-node:gcross.20091111171052.1600:<< Language extensions >>
 -- @nl
 
@@ -15,11 +16,14 @@ module VMPS where
 -- @<< Import needed modules >>
 -- @+node:gcross.20091111171052.1599:<< Import needed modules >>
 import Control.Exception
+import Control.Monad
 
 import Data.Array.Storable
 import Data.Array.Unboxed
+import Data.Function
 import Data.Int
 
+import Foreign.C.String
 import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.Storable
@@ -33,28 +37,30 @@ import System.IO.Unsafe
 -- @+others
 -- @+node:gcross.20091111171052.1593:Utilitity Functions
 -- @+node:gcross.20091111171052.1594:withPinnedArray
-withPinnedArray :: (Ix i, Storable e, IArray UArray e) => UArray i e -> (Ptr e -> IO a) -> IO a
-withPinnedArray arr thunk = 
+withPinnedArray :: (Ix i, Storable e, IArray UArray e) => UArray i e -> (Ptr e -> StorableArray i e -> IO a) -> IO a
+withPinnedArray arr thunk = do
     newListArray (bounds arr) (elems arr)
     >>=
-    (`withStorableArray` thunk)
+    \arr -> withStorableArray arr (\ptr -> thunk ptr arr)
 -- @-node:gcross.20091111171052.1594:withPinnedArray
+-- @+node:gcross.20091111171052.1658:unpinArray
+unpinArray :: (Ix i, Storable e, IArray UArray e) => StorableArray i e -> IO (UArray i e)
+unpinArray arr = liftM2 listArray (getBounds arr) (getElems arr)
+-- @-node:gcross.20091111171052.1658:unpinArray
 -- @-node:gcross.20091111171052.1593:Utilitity Functions
 -- @+node:gcross.20091111171052.1591:Types
 -- @+node:gcross.20091111171052.1596:ComplexArray
-newtype RealArray = RealArray { unwrapRealArray :: UArray Int Double }
-newtype ComplexArray = ComplexArray { unwrapComplexArray :: UArray Int Double }
+newtype ComplexArray = ComplexArray { unwrapComplexArray :: UArray Int Double } deriving (Show)
 
-withPinnedRealArray = withPinnedArray . unwrapRealArray
 withPinnedComplexArray = withPinnedArray . unwrapComplexArray
+
+unpinComplexArray = unpinArray >=> return . ComplexArray
 
 unboxedArrayFromList list = listArray (1,length list) list
 
 complexArrayFromList = ComplexArray . unboxedArrayFromList
-realArrayFromList = RealArray . unboxedArrayFromList
 
 trivial_complex_array = complexArrayFromList [1.0,0.0]
--- @nonl
 -- @-node:gcross.20091111171052.1596:ComplexArray
 -- @+node:gcross.20091111171052.1595:Left/Right Boundaries
 data BoundaryTensor = BoundaryTensor
@@ -63,7 +69,7 @@ data BoundaryTensor = BoundaryTensor
     ,   boundaryData :: !ComplexArray
     }
 
-withPinnedBoundaryTensor :: BoundaryTensor -> (Ptr Double -> IO a) -> IO a
+withPinnedBoundaryTensor :: BoundaryTensor -> (Ptr Double -> StorableArray Int Double -> IO a) -> IO a
 withPinnedBoundaryTensor = withPinnedComplexArray . boundaryData
 
 newtype LeftBoundaryTensor = LeftBoundaryTensor { unwrapLeftBoundaryTensor :: BoundaryTensor }
@@ -84,7 +90,7 @@ data StateSiteTensor = StateSiteTensor
     ,   stateData :: !ComplexArray
     }
 
-withPinnedStateSiteTensor :: StateSiteTensor -> (Ptr Double -> IO a) -> IO a
+withPinnedStateSiteTensor :: StateSiteTensor -> (Ptr Double -> StorableArray Int Double -> IO a) -> IO a
 withPinnedStateSiteTensor = withPinnedComplexArray . stateData
 -- @-node:gcross.20091111171052.1597:State Site Tensor
 -- @+node:gcross.20091111171052.1598:Operator Site Tensor
@@ -104,9 +110,19 @@ withPinnedOperatorSiteTensor operator_site_tensor thunk =
     (withStorableArray . operatorMatrices) operator_site_tensor $ \p_matrices ->
     thunk p_number_of_matrices p_indices p_matrices
 -- @-node:gcross.20091111171052.1598:Operator Site Tensor
+-- @+node:gcross.20091111171052.1657:SelectionStrategy
+data SelectionStrategy = LM | SR
+
+instance Show SelectionStrategy where
+    show LM = "LM"
+    show SR = "SR"
+
+withStrategyAsCString :: SelectionStrategy -> (CString -> IO a) -> IO a
+withStrategyAsCString = withCString . show
+-- @-node:gcross.20091111171052.1657:SelectionStrategy
 -- @-node:gcross.20091111171052.1591:Types
 -- @+node:gcross.20091111171052.1601:Classes
--- @+node:gcross.20091111171052.1602:Connectable
+-- @+node:gcross.20091111171052.1602:Connected
 class Connected a b where
     (<-?->) :: a -> b -> Int
 
@@ -139,11 +155,23 @@ instance Connected OperatorSiteTensor RightBoundaryTensor where
         let d1 = operatorRightBandwidth $ operator_site_tensor
             d2 = boundaryStateBandwidth . unwrapRightBoundaryTensor $ right_boundary_tensor
         in if d1 == d2 then d1 else error $ (printf "Right boundary and state site tensors disagree over the bandwidth dimension! (%i != %i)\n" d1 d2)
--- @-node:gcross.20091111171052.1602:Connectable
+-- @-node:gcross.20091111171052.1602:Connected
+-- @+node:gcross.20091111171052.1664:AlmostEq
+class AlmostEq a where
+    (~=) :: a -> a -> Bool
+
+instance AlmostEq Double where
+    x ~= y = abs (x-y) < 1e-7
+
+instance (AlmostEq a) => AlmostEq [a] where
+    x ~= y = all (uncurry (~=)) $ zip x y
+
+x /~ y = not (x ~= y)
+-- @-node:gcross.20091111171052.1664:AlmostEq
 -- @-node:gcross.20091111171052.1601:Classes
 -- @+node:gcross.20091111171052.1588:Foreign imports
 
--- @+node:gcross.20091111171052.1589:compute_expectation
+-- @+node:gcross.20091111171052.1589:computeExpectation
 foreign import ccall unsafe "compute_expectation_" compute_expectation :: 
     Ptr Int -> -- state left bandwidth dimension
     Ptr Int -> -- state right bandwidth dimension
@@ -165,12 +193,11 @@ computeExpectation left_boundary_tensor state_site_tensor operator_site_tensor r
         cl = left_boundary_tensor <-?-> operator_site_tensor
         cr = operator_site_tensor <-?-> right_boundary_tensor
         d = operator_site_tensor <-?-> state_site_tensor
-    in
-        unsafePerformIO $
-        withPinnedLeftBoundaryTensor left_boundary_tensor $ \p_left_boundary ->
-        withPinnedStateSiteTensor state_site_tensor $ \p_state_site_tensor ->
+    in unsafePerformIO $
+        withPinnedLeftBoundaryTensor left_boundary_tensor $ \p_left_boundary _ ->
+        withPinnedStateSiteTensor state_site_tensor $ \p_state_site_tensor _ ->
         withPinnedOperatorSiteTensor operator_site_tensor $ \p_number_of_matrices p_operator_indices p_operator_matrices ->
-        withPinnedRightBoundaryTensor right_boundary_tensor $ \p_right_boundary ->
+        withPinnedRightBoundaryTensor right_boundary_tensor $ \p_right_boundary _ ->
         with bl $ \p_bl ->
         with br $ \p_br ->
         with cl $ \p_cl ->
@@ -185,7 +212,81 @@ computeExpectation left_boundary_tensor state_site_tensor operator_site_tensor r
                 p_state_site_tensor
                 p_number_of_matrices p_operator_indices p_operator_matrices
                 p_right_boundary
--- @-node:gcross.20091111171052.1589:compute_expectation
+-- @-node:gcross.20091111171052.1589:computeExpectation
+-- @+node:gcross.20091111171052.1656:computeOptimalSiteStateTensor
+foreign import ccall unsafe "optimize_" optimize ::
+    Ptr Int -> -- state left bandwidth dimension
+    Ptr Int -> -- state right bandwidth dimension
+    Ptr Int -> -- operator left bandwidth dimension
+    Ptr Int -> -- operator right bandwidth dimension
+    Ptr Int -> -- physical dimension
+    Ptr Double -> -- left environment
+    Ptr Int -> -- number of matrices
+    Ptr Int32 -> -- sparse operator indices
+    Ptr Double -> -- sparse operator matrices
+    Ptr Double -> -- right environment
+    CString -> -- which eigenvectors to select
+    Ptr Double -> -- tolerance
+    Ptr Int -> -- cap on the number of iterations / number of iterations that were used
+    Ptr Double -> -- initial guess for the state site tensor / where the result will be written
+    IO Int32
+
+computeOptimalSiteStateTensor ::
+    LeftBoundaryTensor ->
+    StateSiteTensor ->
+    OperatorSiteTensor ->
+    RightBoundaryTensor ->
+    SelectionStrategy ->
+    Double ->
+    Int ->
+    Either Int32 (Int,StateSiteTensor)
+computeOptimalSiteStateTensor
+    left_boundary_tensor
+    state_site_tensor
+    operator_site_tensor
+    right_boundary_tensor
+    strategy
+    tolerance
+    maximum_number_of_iterations
+    =
+    let bl = left_boundary_tensor <-?-> state_site_tensor
+        br = state_site_tensor <-?-> right_boundary_tensor
+        cl = left_boundary_tensor <-?-> operator_site_tensor
+        cr = operator_site_tensor <-?-> right_boundary_tensor
+        d = operator_site_tensor <-?-> state_site_tensor
+    in unsafePerformIO $
+        withPinnedLeftBoundaryTensor left_boundary_tensor $ \p_left_boundary _ ->
+        withPinnedStateSiteTensor state_site_tensor $ \p_state_site_tensor state_site_tensor_storable_array ->
+        withPinnedOperatorSiteTensor operator_site_tensor $ \p_number_of_matrices p_operator_indices p_operator_matrices ->
+        withPinnedRightBoundaryTensor right_boundary_tensor $ \p_right_boundary _ ->
+        with bl $ \p_bl ->
+        with br $ \p_br ->
+        with cl $ \p_cl ->
+        with cr $ \p_cr ->
+        with d  $ \p_d  ->
+        withStrategyAsCString strategy $ \p_strategy ->
+        with tolerance $ \p_tolerance ->
+        with maximum_number_of_iterations  $ \p_number_of_iterations -> do
+            optimize
+                p_bl p_br
+                p_cl p_cr
+                p_d
+                p_left_boundary
+                p_number_of_matrices p_operator_indices p_operator_matrices
+                p_right_boundary
+                p_strategy
+                p_tolerance
+                p_number_of_iterations
+                p_state_site_tensor
+            >>=
+            \result ->
+                if result < 0
+                    then return (Left result)
+                    else do
+                        number_of_iterations <- peek p_number_of_iterations
+                        state_site_tensor_array <- unpinComplexArray state_site_tensor_storable_array
+                        return $ Right (number_of_iterations,StateSiteTensor bl br d state_site_tensor_array)
+-- @-node:gcross.20091111171052.1656:computeOptimalSiteStateTensor
 -- @-node:gcross.20091111171052.1588:Foreign imports
 -- @-others
 -- @-node:gcross.20091110205054.1969:@thin VMPS.hs
--- tests/haskell/test.hs
+++ tests/haskell/test.hs
@@ -29,6 +29,9 @@ import VMPS
 -- @-node:gcross.20091111171052.1610:<< Import needed modules >>
 -- @nl
 
+-- @+others
+-- @-others
+
 main = defaultMain
     -- @    << Tests >>
     -- @+node:gcross.20091111171052.1640:<< Tests >>
@@ -47,7 +50,7 @@ main = defaultMain
                 expectation = computeExpectation left_boundary state_site_tensor operator_site_tensor right_boundary
             in assertEqual "is the correct result returned?" 1 expectation
         -- @-node:gcross.20091111171052.1650:trivial, all dimensions 1
-        -- @+node:gcross.20091111171052.1655:trivial, all dimensions 1
+        -- @+node:gcross.20091111171052.1655:trivial, d =2
         ,testCase "trivial, d = 2" $
             assertEqual "are the correct results returned for all calls?"
             [1,1,-1,-1]
@@ -61,10 +64,48 @@ main = defaultMain
                     operator_site_tensor = OperatorSiteTensor 1 1 2 1 operator_indices operator_matrices
                 in computeExpectation left_boundary state_site_tensor operator_site_tensor right_boundary
             ) [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-        -- @-node:gcross.20091111171052.1655:trivial, all dimensions 1
+        -- @-node:gcross.20091111171052.1655:trivial, d =2
         -- @-others
         ]
     -- @-node:gcross.20091111171052.1649:computeExpectation
+    -- @+node:gcross.20091111171052.1659:computeOptimalSiteStateTensor
+    ,testGroup "computeOptimalSiteStateTensor"
+        -- @    @+others
+        -- @+node:gcross.20091111171052.1661:trivial, d = 4
+        [testCase "trivial, d = 4" $
+            let left_boundary = trivial_left_boundary
+                right_boundary = trivial_right_boundary
+                state_site_tensor = StateSiteTensor 1 1 4 . complexArrayFromList $ replicate (4*2) 1
+                operator_indices = unsafePerformIO $ newArray (1,2) 1
+                operator_matrices = unsafePerformIO $ newListArray (1,4*4*2)
+                    [1,0, 0,0, 0,0, 0,0
+                    ,0,0, 1,0, 0,0, 0,0
+                    ,0,0, 0,0, 1,0, 0,0
+                    ,0,0, 0,0, 0,0,-1,0
+                    ]
+                operator_site_tensor = OperatorSiteTensor 1 1 4 1 operator_indices operator_matrices
+                either_result =
+                    computeOptimalSiteStateTensor
+                        left_boundary
+                        state_site_tensor
+                        operator_site_tensor
+                        right_boundary
+                        SR
+                        0
+                        10000
+            in case either_result of
+                Left info -> assertFailure $ "failed with info = " ++ show info
+                Right (_, (StateSiteTensor bl br d (ComplexArray actual_array))) -> do
+                    let components = elems actual_array
+                    assertEqual "is the new left bandwidth dimension the same?" (stateLeftBandwidth state_site_tensor) bl
+                    assertEqual "is the new right bandwidth dimension the same?" (stateRightBandwidth state_site_tensor) br
+                    assertEqual "is the new physical dimension the same?" (statePhysicalDimension state_site_tensor) d
+                    assertBool "are all but the last component of the state zero?" (take 6 components ~= replicate 6 0)
+                    assertBool "are either of the last components non-zero?" (any (/~ 0) . drop 6 $ components)
+        -- @-node:gcross.20091111171052.1661:trivial, d = 4
+        -- @-others
+        ]
+    -- @-node:gcross.20091111171052.1659:computeOptimalSiteStateTensor
     -- @-others
     -- @-node:gcross.20091111171052.1640:<< Tests >>
     -- @nl
