df429b82cf1da22b45c24d081407df497e268e99
gcross <gcross@phys.washington.edu>
2011-02-21 14:33:41 -0800

    Removed a vestigial file.

--- create-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE state_site_tensors
-  ( state_id uuid
-  , site_number integer
-  , physical_dimension smallint
-  , left_bandwidth_dimension smallint
-  , right_bandwidth_dimension smallint
-  , data blob
-  );
