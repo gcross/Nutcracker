2308f5347f75f6335cf71ca9956de075d49a7db2
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-09-02 14:56:37 -0700

    Added support for serializing state sites using Protocol Buffers.

---
+++ includes/protobuf.hpp
@@ -0,0 +1,120 @@
+//@+leo-ver=5-thin
+//@+node:gcross.20110901221152.2649: * @file protobuf.hpp
+//@@language cplusplus
+
+//@+<< License >>
+//@+node:gcross.20110901221152.2650: ** << License >>
+//@+at
+// Copyright (c) 2011, Gregory Crosswhite
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//@@c
+//@-<< License >>
+
+#ifndef NUTCRACKER_PROTOBUF_HPP
+#define NUTCRACKER_PROTOBUF_HPP
+
+//@+<< Includes >>
+//@+node:gcross.20110901221152.2651: ** << Includes >>
+#include <boost/none.hpp>
+#include "nutcracker.pb.h"
+
+#include "tensors.hpp"
+//@-<< Includes >>
+
+//@+others
+//@+node:gcross.20110901221152.2653: ** I/O Operators
+//@+node:gcross.20110901221152.2654: *3* StateSite
+namespace Nutcracker {
+
+    namespace Protobuf {
+
+        template<typename Side> struct normalizationForProtobufOf {};
+
+        template<> struct normalizationForProtobufOf<Left> {
+            static boost::optional<Normalization> const get() { return LEFT; }
+        };
+        
+        template<> struct normalizationForProtobufOf<Middle> {
+            static boost::optional<Normalization> const get() { return MIDDLE; }
+        };
+        
+        template<> struct normalizationForProtobufOf<Right> {
+            static boost::optional<Normalization> const get() { return RIGHT; }
+        };
+        
+        template<> struct normalizationForProtobufOf<None> {
+            static boost::optional<Normalization> const get() { return none; }
+        };
+
+
+        template<typename side> void storeNormalizationInto(StateSite& buffer) {
+            boost::optional<Normalization> const& normalization = normalizationForProtobufOf<side>::get();
+            if(normalization) {
+                buffer.set_normalization(*normalization);
+            } else {
+                buffer.clear_normalization();
+            }
+        }
+
+        boost::optional<std::string> const& loadNormalizationFrom(StateSite const& buffer) {
+            if(buffer.has_normalization()) {
+                switch(buffer.normalization()) {
+                    case MIDDLE: return normalizationOf<Middle>::value;
+                    case LEFT: return normalizationOf<Left>::value;
+                    case RIGHT: return normalizationOf<Right>::value;
+                }
+            }
+            return normalizationOf<None>::value;
+        }
+
+    }
+
+}
+
+template<typename side> void operator<<(Nutcracker::Protobuf::StateSite& buffer, Nutcracker::StateSite<side> const& tensor) {
+    using namespace Nutcracker;
+    Protobuf::storeNormalizationInto<side>(buffer);
+    buffer.set_physical_dimension(tensor.physicalDimension());
+    buffer.set_left_dimension(tensor.leftDimension());
+    buffer.set_right_dimension(tensor.rightDimension());
+    google::protobuf::RepeatedField<double>& data = *buffer.mutable_data();
+    data.Clear();
+    data.Reserve(2*tensor.size());
+    BOOST_FOREACH(complex<double> const& x, tensor) {
+        (*data.AddAlreadyReserved()) = x.real();
+        (*data.AddAlreadyReserved()) = x.imag();
+    }
+}
+
+template<typename side> void operator>>(Nutcracker::Protobuf::StateSite const& buffer, Nutcracker::StateSite<side>& tensor) {
+    using namespace Nutcracker;
+    assertNormalizationIs<side>(Protobuf::loadNormalizationFrom(buffer));
+    PhysicalDimension const physical_dimension(buffer.physical_dimension());
+    LeftDimension     const left_dimension    (buffer.left_dimension());
+    RightDimension    const right_dimension   (buffer.right_dimension());
+    StateSite<side> state_site_tensor(physical_dimension,left_dimension,right_dimension);
+    google::protobuf::RepeatedField<double> const& data = buffer.data();
+    assert(state_site_tensor.size()*2 == (unsigned int)data.size());
+    google::protobuf::RepeatedField<double>::const_iterator iter = data.begin();
+    BOOST_FOREACH(std::complex<double>& x, state_site_tensor) {
+        x.real() = *(iter++);
+        x.imag() = *(iter++);
+    }
+    tensor = boost::move(state_site_tensor);
+}
+//@+node:gcross.20110901221152.2676: *3* State
+//@+at
+// template<typename side> void operator<<(Nutcracker::Protobuf::StateSiteTensor& buffer, Nutcracker::State const& tensor);
+// 
+// template<typename side> void operator>>(Nutcracker::Protobuf::StateSiteTensor const& buffer, Nutcracker::State& tensor)
+//@-others
+
+#endif
+//@-leo
---
+++ tests/protobuf.cpp
@@ -0,0 +1,146 @@
+//@+leo-ver=5-thin
+//@+node:gcross.20110901221152.2684: * @file protobuf.cpp
+//@@language cplusplus
+
+//@+<< License >>
+//@+node:gcross.20110901221152.2685: ** << License >>
+//@+at
+// Copyright (c) 2011, Gregory Crosswhite
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//@@c
+//@-<< License >>
+
+//@+<< Includes >>
+//@+node:gcross.20110901221152.2686: ** << Includes >>
+#include <illuminate.hpp>
+
+#include "protobuf.hpp"
+
+#include "test_utils.hpp"
+//@-<< Includes >>
+
+//@+others
+//@+node:gcross.20110901221152.2687: ** Tests
+TEST_SUITE(Protobuf) {
+
+//@+others
+//@+node:gcross.20110901221152.2691: *3* StateSite
+TEST_SUITE(StateSite) {
+
+//@+others
+//@+node:gcross.20110901221152.2692: *4* data
+TEST_CASE(data) {
+    RNG random;
+
+    REPEAT(10) {
+        PhysicalDimension const physical_dimension(random);
+        LeftDimension const left_dimension(random);
+        RightDimension const right_dimension(random);
+
+        StateSite<None> state_site_tensor_1
+            (physical_dimension
+            ,left_dimension
+            ,right_dimension
+            ,fillWithGenerator(random.randomComplexDouble)
+            );
+
+        Protobuf::StateSite buffer;
+        buffer << state_site_tensor_1;
+
+        StateSite<None> state_site_tensor_2 = StateSite<None>
+            (PhysicalDimension(random)
+            ,LeftDimension(random)
+            ,RightDimension(random)
+            ,fillWithGenerator(random.randomComplexDouble)
+            );
+        buffer >> state_site_tensor_2;
+
+        checkSiteTensorsEqual(state_site_tensor_1,state_site_tensor_2);
+    }
+}
+//@+node:gcross.20110901221152.2693: *4* normalization
+TEST_CASE(normalization) {
+    Protobuf::StateSite
+        left_buffer,
+        middle_buffer,
+        right_buffer,
+        none_buffer;
+
+    left_buffer << StateSite<Left>::trivial;
+    middle_buffer << StateSite<Middle>::trivial;
+    right_buffer << StateSite<Right>::trivial;
+    none_buffer << StateSite<None>::trivial;
+
+    StateSite<Left> left_site;
+    StateSite<Middle> middle_site;
+    StateSite<Right> right_site;
+    StateSite<None> none_site;
+
+    left_buffer >> none_site;
+    middle_buffer >> none_site;
+    right_buffer >> none_site;
+
+    left_buffer >> left_site;
+    middle_buffer >> middle_site;
+    right_buffer >> right_site;
+
+    try {
+        left_buffer >> middle_site;
+        FAIL("A middle-normalized state site accepted a left-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        left_buffer >> right_site;
+        FAIL("A right-normalized state site accepted a left-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        middle_buffer >> left_site;
+        FAIL("A left-normalized state site accepted an middle-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        middle_buffer >> right_site;
+        FAIL("A right-normalized state site accepted a middle-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        right_buffer >> left_site;
+        FAIL("A left-normalized state site accepted a right-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        right_buffer >> middle_site;
+        FAIL("A middle-normalized state site accepted a right-normalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        none_buffer >> left_site;
+        FAIL("A left-normalized state site accepted an unnormalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        none_buffer >> middle_site;
+        FAIL("A middle-normalized state site accepted an unnormalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+
+    try {
+        none_buffer >> right_site;
+        FAIL("A right-normalized state site accepted an unnormalized tensor!");
+    } catch (WrongTensorNormalizationException const& _) {}
+}
+//@-others
+
+}
+//@-others
+
+}
+//@-others
+//@-leo
