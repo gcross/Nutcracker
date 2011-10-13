7f828a592ce2642cdbe56627279622c83f7308aa
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-10-13 16:21:58 +1000

    Factored the submatrix code out of computeNormalizationMatrix.

--- flatland/grid.py
+++ flatland/grid.py
@@ -28,19 +28,27 @@ class Grid:
         self.center.data[:,0,0,0,0] = array([1] + [0]*(physical_dimension-1))
     #@+node:gcross.20111009193003.5260: *4* computeNormalizationMatrix
     def computeNormalizationMatrix(self):
-        side_boundaries = [side.formBoundary().absorbCounterClockwiseCornerBoundary(corner.formBoundary()) for (side,corner) in zip(self.sides,self.corners)]
-        final_dimension = self.physical_dimension*product([side.inward_dimension for side in self.sides])
+        submatrix = self.computeNormalizationSubmatrix()
         return (
              tensordot(
-                 identity(self.physical_dimension),
-                 tensordot(
-                    side_boundaries[0].absorbCounterClockwiseSideBoundary(side_boundaries[1]).data,
-                    side_boundaries[2].absorbCounterClockwiseSideBoundary(side_boundaries[3]).data,
-                    ((1,0),(0,1))
-                 ),
-                 ((),())
+                identity(self.physical_dimension),
+                submatrix,
+                ((),()),
              )
-            .transpose(0,2,4,1,3,5)
+            .transpose(0,2,1,3)
+            .reshape(*((self.physical_dimension*submatrix.shape[0],)*2))
+        )
+    #@+node:gcross.20111013080525.1259: *4* computeNormalizationSubmatrix
+    def computeNormalizationSubmatrix(self):
+        side_boundaries = [side.formBoundary().absorbCounterClockwiseCornerBoundary(corner.formBoundary()) for (side,corner) in zip(self.sides,self.corners)]
+        final_dimension = product([side.inward_dimension for side in self.sides])
+        return (
+             tensordot(
+                side_boundaries[0].absorbCounterClockwiseSideBoundary(side_boundaries[1]).data,
+                side_boundaries[2].absorbCounterClockwiseSideBoundary(side_boundaries[3]).data,
+                ((1,0),(0,1))
+             )
+            .transpose(0,2,1,3)
             .reshape(final_dimension,final_dimension)
         )
     #@+node:gcross.20111013080525.1244: *4* contract
