cd8be7e27deb0f02e629ea106cf77ca110ae0c95
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-01-11 18:56:38 +1000

    Folded data.py functionality into utils.py

--- nutcracker/utils.py
+++ nutcracker/utils.py
@@ -1,4 +1,5 @@
 # Imports {{{
+from collections import namedtuple
 from copy import copy
 from numpy import array, complex128, dot, ndarray, product, sqrt, tensordot, zeros
 from numpy.linalg import eigh, norm, qr, svd
@@ -6,6 +7,12 @@ from numpy.random import rand
 
 from . import core
 # }}}
+
+# Types {{{
+TensorNumberAndIndex = namedtuple("TensorNumberAndIndex",["tensor","index"])
+# }}}
+
+# Functions {{{
 
 def appended(vector,entry): # {{{
     copy_of_vector = list(vector)
@@ -77,6 +84,42 @@ def computePostContractionIndexMap(old_number_of_dimensions,indices_being_contra
         old_to_new_index_map[current_old_index] = current_new_index + offset
         current_old_index += 1
     return old_to_new_index_map
+# }}}
+
+def contract(arrays,array_axes): # {{{
+    return tensordot(data_1,data_2,(axes_1,axes_2))
+# }}}
+
+def contractAndTranspose(arrays,array_axes,new_order): # {{{
+    return contractAndTransposeAndJoin(arrays,array_axes,[[x] for x in new_order])
+# }}}
+
+def contractAndTransposeAndJoin(arrays,array_axes,new_grouped_order): # {{{
+    old_to_new_maps = map(computePostContractionIndexMap,array_axes)
+    shapes = [array.shape for array in arrays]
+    contracted_axes_sets = map(frozenset,array_axes)
+    non_contracted_axes_sets = [
+        set(xrange(array.ndim)) - contracted_axes_set
+        for (array,contracted_axes_set) in zip (arrays,contracted_axes_sets)
+    ]
+    new_order = []
+    new_shape = []
+    for group in new_grouped_order:
+        dimension = 1
+        for tensor_number, index in group:
+            if tensor_number not in [0,1]:
+                raise ValueError("the tensor index must be either 0 or 1, not {}".format(index))
+            if index in contracted_axes[tensor_number]:
+                raise ValueError("index {} of tensor {} appears both in the list of indices to contract and in the final order".format(index,tensor_number))
+            if index not in non_contracted_axes_sets[tensor_number]:
+                raise ValueError("index {} of tensor {} appears twice in the final ordering".format(index,tensor_number))
+            non_contracted_axes_sets[tensor_number].remove(index)
+            new_order.append(old_to_new_maps[tensor_number][index])
+            dimension *= shapes[tensor_number][index]
+        new_shape.append(dimension)
+    if any(non_contracted_axes_sets):
+        raise ValueError("the follow indices of respectively tensor 0 and tensor 1 appear nowhere in either the axes to contract or in the final ordering: {}, {}".format(*non_contracted_axes_sets)
+    return tensordot(arrays[0],arrays[1],array_axes).transpose(new_order).reshape(new_shape)
 # }}}
 
 def constructFilterFrom(keep=None,threshold=None): # {{{
@@ -398,8 +441,12 @@ def withoutIndex(vector,index): # {{{
     return copy_of_vector
 # }}}
 
+# }}}
+
 # Exports {{{
 __all__ = [
+    "TensorNumberAndIndex",
+
     "appended",
     "basisVector",
     "CCW",
@@ -413,6 +460,9 @@ __all__ = [
     "compressHermitianConnectionUsingFirstTensorOnlyBetween",
     "compressHermitianConnectionUsingFirstTensorOnlyBetweenTensors",
     "computePostContractionIndexMap",
+    "contract",
+    "contractAndTranspose",
+    "contractAndTransposeAndJoin",
     "CW",
     "firstIndexBelowMagnitude",
     "formContractor",
