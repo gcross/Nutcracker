f55e237d298d4d84f710a453075a4f6e32aede46
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-04-13 18:29:50 +1000

    Fixed the glitch introduced by the new version of Boost.

--- sources/common/version.cpp
+++ sources/common/version.cpp
@@ -1,7 +1,5 @@
+#include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/range/adaptor/transformed.hpp>
-#include <boost/range/algorithm/copy.hpp>
-#include <boost/range/iterator_range.hpp>
 #include <boost/tokenizer.hpp>
 #include <iterator>
 
@@ -15,13 +13,15 @@ namespace Nutcracker {
 static boost::container::vector<unsigned int> decodeVersionString(std::string const& version_string) {
     boost::container::vector<unsigned int> version;
 
-    boost::copy(
+    BOOST_FOREACH(
+        std::string const& component_as_string,
         boost::tokenizer<boost::char_separator<char> >(
             version_string,
             boost::char_separator<char>(".")
-        ) | boost::adaptors::transformed(&boost::lexical_cast<unsigned int,std::string>),
-        std::back_inserter(version)
-    );
+        )
+    ) {
+        version.push_back(boost::lexical_cast<unsigned int,std::string>(component_as_string));
+    }
 
     return boost::move(version);
 }
